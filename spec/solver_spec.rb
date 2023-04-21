c9f76311255913f6c702aa067276b43f6569adc0
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-04-21 07:00:08 +0300

    Add factorial method test

---
+++ spec/solver_spec.rb
@@ -0,0 +1,16 @@
+require './solver'
+require 'rspec'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  describe '#factorial' do
+    it 'successfully implements the mathematical factorial method' do
+      expect(@solver.factorial(5)).to eq 120
+      expect(@solver.factorial(-1)).to eq 'Wrong input, please provide positive integer numbers!'
+      expect(@solver.factorial(0)).to eq 1
+    end
+  end
+end
